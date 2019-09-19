b15f4431c14dfc62d0a9eb253ab3968991f764b1
jaboud <jaboud94@gmail.com>
2019-09-19 13:11:32 +1000

    Created static method using integer arguments

--- HackerRank-Mini-Max-Sum/Program.cs
+++ HackerRank-Mini-Max-Sum/Program.cs
@@ -4,6 +4,11 @@ namespace HackerRank_Mini_Max_Sum
 {
     class Program
     {
+        static void miniMaxSum(int[] arr)
+        {
+
+
+        }
         static void Main(string[] args)
         {
             int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))

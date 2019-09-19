845477ecd336f1be75c66637e52351fe10fc5fd6
jaboud <jaboud94@gmail.com>
2019-09-19 13:15:03 +1000

    Renamed MiniMaxSum static method,

--- HackerRank-Mini-Max-Sum/Program.cs
+++ HackerRank-Mini-Max-Sum/Program.cs
@@ -4,16 +4,15 @@ namespace HackerRank_Mini_Max_Sum
 {
     class Program
     {
-        static void miniMaxSum(int[] arr)
+        static void MiniMaxSum(int[] arr)
         {
-
 
         }
         static void Main(string[] args)
         {
             int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
         ;
-            miniMaxSum(arr);
+            MiniMaxSum(arr);
         }
     }
 }

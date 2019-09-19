c2e9577368ba2c68cafd15540645460fb880078e
jaboud <jaboud94@gmail.com>
2019-09-19 13:08:54 +1000

    Initial solution creation

---
+++ HackerRank-Mini-Max-Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
